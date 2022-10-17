db5acf6eaa99889ef87b9e5b0b4e2f772145ca4a
Ondairos <ondairos@gmail.com>
2022-10-17 17:34:01 +0300

    added index and new-meetup pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
+import Layout from '../components/layout/Layout'
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'First Meetup',
+        image: 'https://images.pexels.com/photos/13816113/pexels-photo-13816113.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        address: 'Some address',
+        description: 'This is a first meetup'
+
+    },
+    {
+        id: 'm2',
+        title: 'Second Meetup',
+        image: 'https://images.unsplash.com/photo-1665899689931-6638f06268b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1208&q=80',
+        address: 'Some address',
+        description: 'This is the second meetup'
+
+    },
+    {
+        id: 'm3',
+        title: 'Third Meetup',
+        image: 'https://images.unsplash.com/photo-1665899689931-6638f06268b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1208&q=80',
+        address: 'Some address',
+        description: 'This is the third meetup meetup'
+
+    }
+]
+
+
 const HomePage = () => {
     return (
-        <div>
-            <h1>The Home Page</h1>
-        </div>
+
+        <MeetupList meetups={DUMMY_MEETUPS}>
+        </MeetupList>
+
     )
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData)
+    }
+    return (
+        <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
+    )
+}
+
+export default NewMeetupPage
