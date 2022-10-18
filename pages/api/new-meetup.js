fa3b0f95c0a3a3169185bd9c22244103d8c164b9
Ondairos <ondairos@gmail.com>
2022-10-18 13:33:22 +0300

    added MongoDB to api and response

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,22 @@
+import { MongoClient } from 'mongodb'
+
 // /api/new-meetup
 // Only POST requests
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+        // const { title, image, address, description } = data;
+
+        const client = MongoClient.connect();
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data); //instert one new docoume to this collection in mongo db
+        client.close(); //close database connection with mongodb
+
+        //send respond
+        res.status(201), json({ message: 'Meetup inserted!' });
     }
 }
 
